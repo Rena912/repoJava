1e30ec77d2fc94d1e13695d78935c333b093854f
RenaP <renatopirri@gmail.com>
2025-07-04 00:08:26 +0200

    add invertiarray

---
+++ src/InvertiArray.java
@@ -0,0 +1,12 @@
+
+
+public class InvertiArray {
+    
+    public static void main(String[] args) {
+        
+        int [] numeri ={1,2,3,4,5,6,7};
+        for (int i = numeri.length; i > 0; i--) {
+        System.out.println(i);
+        }
+    }
+}
